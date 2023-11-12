6cdb168e8088127f63673add72793e712015da19
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2023-11-13 00:59:10 +0300

    Add project files.

---
+++ Business/Abstract/IProduct.cs
@@ -0,0 +1,7 @@
+﻿namespace Business.Abstract
+{
+    public interface IProduct
+    {
+        bool ProductName { get; }
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Abstract;
+using Entities.Concrete.InMemory;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<IProduct> GetAll()
+        {
+            //iş kodları
+            return _productDal.GetAll();
+        }
+    }
+}
